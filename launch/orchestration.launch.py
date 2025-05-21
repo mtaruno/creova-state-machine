6f7f0c42097822414c04c4f0372cfd2c9123f8b1
mtaruno <matthew.taruno@gmail.com>
2025-05-21 00:48:12 -0700

    update integration

--- launch/orchestration.launch.py
+++ launch/orchestration.launch.py
@@ -4,9 +4,34 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='orchestration_node',
-            executable='robot_brain_node',
-            name='robot_brain',
+            package='creova_state_machine',
+            executable='orchestration_node',
+            name='orchestration_node',
             output='screen'
-        )
+        ),
+        Node(
+            package='creova_state_machine',
+            executable='navigation_node',
+            name='navigation_node',
+            output='screen'
+        ),
+        Node(
+            package='creova_state_machine',
+            executable='manipulation_node',
+            name='manipulation_node',
+            output='screen'
+        ),
+        Node(
+            package='creova_state_machine',
+            executable='perception_node',
+            name='perception_node',
+            output='screen'
+        ),
+        Node(
+            package='creova_state_machine',
+            executable='task_input_node',
+            name='task_input_node',
+            output='screen'
+        ),
+
     ])
